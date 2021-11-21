5da2a81d9ecd4fe70c81f9627d9de9fe671e03d9
Thanh Nguyen <vnscriptkid@gmail.com>
2021-11-21 13:43:04 +0700

    feat: hash password

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,10 +1,23 @@
+import bcrypt from "bcrypt";
+
 export default {
   Query: {
     allUsers: (parent, args, { models }) => models.User.findAll(),
     getUser: (parent, { id }, { models }) => models.User.findById(id),
   },
   Mutation: {
-    createUser: async (parent, args, { models }) =>
-      models.User.create({ ...args }),
+    register: async (parent, { username, email, password }, { models }) => {
+      try {
+        const hashedPassword = await bcrypt.hash(
+          password,
+          Number(process.env.SALT_ROUNDS)
+        );
+        await models.User.create({ username, email, password: hashedPassword });
+        return true;
+      } catch (e) {
+        console.error(e);
+        return false;
+      }
+    },
   },
 };
