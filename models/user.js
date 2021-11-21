33b045924d056b2eef30cf90f84a8ba83e9eff3c
Thanh Nguyen <vnscriptkid@gmail.com>
2021-11-21 16:27:49 +0700

    feat: user model validations

--- models/user.js
+++ models/user.js
@@ -7,12 +7,36 @@ export default (sequelize, DataTypes) => {
       username: {
         type: DataTypes.STRING,
         unique: true,
+        validate: {
+          isAlphanumeric: {
+            args: true,
+            msg: "The username can only contain letters and numbers",
+          },
+          len: {
+            args: [3, 25],
+            msg: "The username needs to be between 3 and 25 characters long",
+          },
+        },
       },
       email: {
         type: DataTypes.STRING,
         unique: true,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "Invalid email",
+          },
+        },
       },
-      password: DataTypes.STRING,
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [5, 20],
+            msg: "The password needs to be between 5 and 20 characters long",
+          },
+        },
+      },
     },
     {
       hooks: {
