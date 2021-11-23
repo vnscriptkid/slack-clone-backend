4e0e7d75910b75fa47dac1ff62ede17a149ea375
Thanh Nguyen <vnscriptkid@gmail.com>
2021-11-24 01:12:41 +0700

    feat: auth middleware

--- resolvers/team.js
+++ resolvers/team.js
@@ -1,20 +1,23 @@
 import formatErrors from "../formatErrors";
+import requireAuth from "../permissions";
 
 export default {
   Mutation: {
-    createTeam: async (parent, { name }, { models, user }) => {
-      try {
-        await models.Team.create({ name, owner: user.id });
-        return {
-          ok: true,
-        };
-      } catch (e) {
-        console.error(e);
-        return {
-          ok: false,
-          errors: formatErrors(e, models),
-        };
+    createTeam: requireAuth.createResolver(
+      async (parent, { name }, { models, user }) => {
+        try {
+          await models.Team.create({ name, owner: user.id });
+          return {
+            ok: true,
+          };
+        } catch (e) {
+          console.error(e);
+          return {
+            ok: false,
+            errors: formatErrors(e, models),
+          };
+        }
       }
-    },
+    ),
   },
 };
