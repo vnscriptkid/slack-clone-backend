401e13fa5df66d8e57d7a6720fedc72c7365a26c
Thanh Nguyen <vnscriptkid@gmail.com>
2021-11-26 14:57:46 +0700

    chore: turn off sequelize warning

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ import Sequelize from "sequelize";
 
 const sequelize = new Sequelize("slack-dev", "postgres", "123456", {
   dialect: "postgres",
+  operatorsAliases: Sequelize.Op,
   define: {
     underscored: true,
   },
