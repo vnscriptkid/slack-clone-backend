c9da2b462053b8b397bc0b12552e5f4fe7c4bba3
Thanh Nguyen <vnscriptkid@gmail.com>
2021-11-27 20:05:26 +0700

    feat: add team member

--- resolvers/team.js
+++ resolvers/team.js
@@ -32,6 +32,62 @@ export default {
         }
       }
     ),
+    addTeamMember: requireAuth.createResolver(
+      async (parent, { email, teamId }, { models, user }) => {
+        try {
+          const teamPromise = models.Team.findOne(
+            { where: { id: teamId } },
+            { raw: true }
+          );
+
+          const userToAddPromise = models.User.findOne(
+            { where: { email } },
+            { raw: true }
+          );
+
+          const [team, userToAdd] = await Promise.all([
+            teamPromise,
+            userToAddPromise,
+          ]);
+
+          if (team.owner !== user.id) {
+            return {
+              ok: false,
+              errors: [
+                {
+                  path: "email",
+                  message: "You cannot add members to the team",
+                },
+              ],
+            };
+          }
+
+          if (!userToAdd) {
+            return {
+              ok: false,
+              errors: [
+                {
+                  path: "email",
+                  message: "Could not find user with this email",
+                },
+              ],
+            };
+          }
+
+          await models.Member.create({ userId: userToAdd.id, teamId });
+
+          return {
+            ok: true,
+          };
+        } catch (e) {
+          console.error(e);
+          return {
+            ok: false,
+            errors: formatErrors(e, models),
+          };
+        }
+      }
+    ),
   },
   Team: {
     channels: ({ id }, args, { models }) =>
