b47afbd78734d9a03cc7001a4bf0c588fdcf1a93
Thanh Nguyen <vnscriptkid@gmail.com>
2021-11-28 18:14:55 +0700

    feat: only team owner can create channel

--- resolvers/channel.js
+++ resolvers/channel.js
@@ -1,29 +1,44 @@
 import formatErrors from "../formatErrors";
+import requireAuth from "../permissions";
 
 export default {
   Mutation: {
-    createChannel: async (
-      parent,
-      { teamId, name, public: isPublic },
-      { models }
-    ) => {
-      try {
-        const channel = await models.Channel.create({
-          teamId,
-          name,
-          public: isPublic,
-        });
-        return {
-          ok: true,
-          channel,
-        };
-      } catch (e) {
-        console.error(e);
-        return {
-          ok: false,
-          errors: formatErrors(e, models),
-        };
+    createChannel: requireAuth.createResolver(
+      async (parent, { teamId, name, public: isPublic }, { models, user }) => {
+        const team = await models.Team.findOne({ where: { id: teamId } });
+        const isOwner = team.owner === user.id;
+
+        if (!isOwner) {
+          return {
+            ok: false,
+            errors: [
+              {
+                path: "name",
+                message:
+                  "You have to be the owner of the team to create channel.",
+              },
+            ],
+          };
+        }
+
+        try {
+          const channel = await models.Channel.create({
+            teamId,
+            name,
+            public: isPublic,
+          });
+          return {
+            ok: true,
+            channel,
+          };
+        } catch (e) {
+          console.error(e);
+          return {
+            ok: false,
+            errors: formatErrors(e, models),
+          };
+        }
       }
-    },
+    ),
   },
 };
