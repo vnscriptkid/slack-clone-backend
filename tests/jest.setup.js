9b819220a1b70f5a77f5c46ee744e7a74b12fcf5
Thanh Nguyen <vnscriptkid@gmail.com>
2021-12-18 11:02:40 +0700

    feat(test): clear tables each test

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,1 +1,9 @@
+import models from "../models";
+
 global.url = "http://localhost:4001/graphql";
+
+beforeEach(async () => {
+  await models.User.destroy({ truncate: { cascade: true }, where: {} });
+  await models.Channel.destroy({ truncate: { cascade: true }, where: {} });
+  await models.Team.destroy({ truncate: { cascade: true }, where: {} });
+});
--- tests/jestGlobalSetup.js
+++ tests/jestGlobalSetup.js
@@ -4,6 +4,7 @@ let server;
 
 export default async () => {
   console.log("\nhello, this is just before tests start running");
+
   if (!server) {
     server = await startApolloServer();
   }
