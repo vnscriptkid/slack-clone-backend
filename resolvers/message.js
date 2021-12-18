a46d5af256704ddcd5e8b7f704d446bb93a2385a
Thanh Nguyen <vnscriptkid@gmail.com>
2021-12-18 11:05:31 +0700

    feat(message): must auth to create message

--- resolvers/message.js
+++ resolvers/message.js
@@ -1,17 +1,24 @@
+import requireAuth from "../permissions";
+
 export default {
+  Query: {
+    messages: async (parent, args, { models, user }) => [],
+  },
   Mutation: {
-    createMessage: async (parent, { channelId, text }, { models, user }) => {
-      try {
-        await models.Message.create({
-          userId: user.id,
-          text,
-          channelId,
-        });
-        return true;
-      } catch (e) {
-        console.error(e);
-        return false;
+    createMessage: requireAuth.createResolver(
+      async (parent, { channelId, text }, { models, user }) => {
+        try {
+          await models.Message.create({
+            userId: user.id,
+            text,
+            channelId,
+          });
+          return true;
+        } catch (e) {
+          console.error(e);
+          return false;
+        }
       }
-    },
+    ),
   },
 };
