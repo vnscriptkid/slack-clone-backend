2ca8c5d2cf9615a98aa5e333d583ba0bba2a0853
Thanh Nguyen <vnscriptkid@gmail.com>
2021-12-18 11:04:21 +0700

    chore(sequelize): name of db from env

--- models/index.js
+++ models/index.js
@@ -1,12 +1,17 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize("slack-dev", "postgres", "123456", {
-  dialect: "postgres",
-  operatorsAliases: Sequelize.Op,
-  define: {
-    underscored: true,
-  },
-});
+const sequelize = new Sequelize(
+  process.env.TEST_DB || "slack-dev",
+  "postgres",
+  "123456",
+  {
+    dialect: "postgres",
+    operatorsAliases: Sequelize.Op,
+    define: {
+      underscored: true,
+    },
+  }
+);
 
 const models = {
   User: sequelize.import("./user"),
