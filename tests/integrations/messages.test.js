d51993c35259d51f320b22aee37de95c7f4ddf3b
Thanh Nguyen <vnscriptkid@gmail.com>
2021-12-18 11:06:41 +0700

    test(message): create

---
+++ tests/integrations/messages.test.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+import models from "../../models";
+import authReq from "../utils/authRequest";
+
+describe("messages resolvers", () => {
+  test("query messages of channel", async () => {
+    const res = await axios.post(url, {
+      query: `
+        query {
+          messages(channelId: 1) {
+            id
+            text
+            createdAt
+            user {
+              id
+              username
+            }
+          }
+        }
+      `,
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test("send message to channel", async () => {
+    const text = "Test message";
+
+    const res = await authReq({
+      query: `
+      mutation {
+        createTeam(name: "team-x") {
+          ok
+          team {
+            name
+          }
+        }
+      }
+          `,
+    });
+
+    expect(res.status).toBe(200);
+
+    const team = await models.Team.findOne({ where: { name: "team-x" } });
+
+    const channel = await models.Channel.findOne({
+      where: { teamId: team.id },
+    });
+
+    const query = `
+    mutation {
+      createMessage(channelId: ${channel.id}, text: "${text}") 
+    }
+  `;
+
+    const res2 = await authReq({
+      query,
+    });
+
+    expect(res2.status).toBe(200);
+
+    const message = await models.Message.findOne({
+      where: { channel_id: channel.id },
+      attributes: { exclude: ["created_at", "updated_at"] },
+    });
+
+    expect(message.toJSON()).toMatchInlineSnapshot(`
+Object {
+  "channelId": 16,
+  "id": 9,
+  "text": "Test message",
+  "userId": 16,
+}
+`);
+  });
+});
